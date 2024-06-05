9ecc10cd6c6623756ac8d9af100e06f687077cbe
Seoyeon Park <110394722+syss220211@users.noreply.github.com>
2024-06-05 22:28:27 +0900

    💄 [Design] 색상 디자인 시스템 추가 (#12)

---
+++ Projects/DesignSystem/Sources/Color/ColorView.swift
@@ -0,0 +1,20 @@
+//
+//  ColorView.swift
+//  DesignSystem
+//
+//  Created by 박서연 on 2024/06/05.
+//  Copyright © 2024 iOS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ColorView: View {
+    var body: some View {
+        Text("test")
+            .foregroundStyle(Color.information)
+    }
+}
+
+#Preview {
+    ColorView()
+}
