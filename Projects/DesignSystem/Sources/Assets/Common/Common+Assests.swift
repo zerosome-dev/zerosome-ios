87db988519b76d3068a4e29277139525a50433ff
Seoyeon Park <110394722+syss220211@users.noreply.github.com>
2024-06-18 15:28:30 +0900

    :bento: [Design] Add Tabbar Assest (#19)
    * 💄 [Design] 색상 디자인 시스템 추가
    
    * :bento: [Design] Add Tabbar Assests
    
    * :bento: Add Font
    
    * :adhesive_bandage: [Fix] fix merge error
    
    - conflict 수정하면서 발생한 merge error 수정

---
+++ Projects/DesignSystem/Sources/Assets/Common/Common+Assests.swift
@@ -0,0 +1,13 @@
+//
+//  Common+Assests.swift
+//  DesignSystem
+//
+//  Created by 박서연 on 2024/06/18.
+//  Copyright © 2024 iOS. All rights reserved.
+//
+
+import SwiftUI
+
+public struct ZerosomeAsset {
+    public static let zero_progress = DesignSystemAsset.Assets.progressGray.swiftUIImage
+}
