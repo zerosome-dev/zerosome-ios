aaf83e26770a87f7ce55ea0575ce00035eec02f6
Park Seo Yeon <syss220211@gmail.com>
2024-06-19 10:50:50 +0900

    ✨ [Feat] Generate Login & Term View
    - 로그인 및 약관 화면 구현
    - 백버튼 미적용
    - 모두 동의 액션에 대한 의논 필요

--- Projects/DesignSystem/Sources/Assets/Common/Common+Assests.swift
+++ Projects/DesignSystem/Sources/Assets/Common/Common+Assests.swift
@@ -13,4 +13,8 @@ public struct ZerosomeAsset {
     
     public static let ic_kakako = DesignSystemAsset.Assets.icKakao.swiftUIImage
     public static let ic_apple = DesignSystemAsset.Assets.icApple.swiftUIImage
+    public static let ic_check_circle_gray = DesignSystemAsset.Assets.icCircleCheckGray.swiftUIImage
+    public static let ic_check_circle_primary = DesignSystemAsset.Assets.icCircleCheckPrimary.swiftUIImage
+    
+    public static let ic_back_button = DesignSystemAsset.Assets.icArrowBackGray.swiftUIImage
 }
