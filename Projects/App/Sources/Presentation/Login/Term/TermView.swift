2fba81d941147eed4b62cfe5f080259ae0cf72fd
Park Seo Yeon <syss220211@gmail.com>
2024-06-19 10:01:19 +0900

    ✨[Feat] Main Login View

---
+++ Projects/App/Sources/Presentation/Login/Term/TermView.swift
@@ -0,0 +1,19 @@
+//
+//  TermView.swift
+//  App
+//
+//  Created by 박서연 on 2024/06/19.
+//  Copyright © 2024 iOS. All rights reserved.
+//
+
+import SwiftUI
+
+struct TermView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TermView()
+}
--- Projects/DesignSystem/Sources/Assets/Common/Common+Assests.swift
+++ Projects/DesignSystem/Sources/Assets/Common/Common+Assests.swift
@@ -10,4 +10,7 @@ import SwiftUI
 
 public struct ZerosomeAsset {
     public static let zero_progress = DesignSystemAsset.Assets.progressGray.swiftUIImage
+    
+    public static let ic_kakako = DesignSystemAsset.Assets.icKakao.swiftUIImage
+    public static let ic_apple = DesignSystemAsset.Assets.icApple.swiftUIImage
 }
