adf7037f9805b6b976f80a4401444b56857b534c
Park Seo Yeon <syss220211@gmail.com>
2024-06-20 21:13:14 +0900

    :truck: [Feat] modifier folder structure & set network
    - 폴더 구조 정리
    - 기초 네트워크 작업

---
+++ Projects/App/Sources/Application/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  App
+//
+//  Created by 박서연 on 2024/06/20.
+//  Copyright © 2024 iOS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) {}
+}
