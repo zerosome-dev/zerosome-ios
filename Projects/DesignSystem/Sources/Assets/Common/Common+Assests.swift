b18253043344ab4b53eecf855a6bded9a8250aa5
Park Seo Yeon <syss220211@gmail.com>
2024-06-30 19:16:37 +0900

    🍱 [Design] Add Common icon

--- Projects/DesignSystem/Sources/Assets/Common/Common+Assests.swift
+++ Projects/DesignSystem/Sources/Assets/Common/Common+Assests.swift
@@ -10,4 +10,11 @@ import SwiftUI
 
 public struct ZerosomeAsset {
     public static let zero_progress = DesignSystemAsset.Assets.progressGray.swiftUIImage
+    
+    public static let ic_star_fill = DesignSystemAsset.Assets.icStarFill.swiftUIImage
+    public static let ic_star_empty = DesignSystemAsset.Assets.icStarEmpty.swiftUIImage
+    public static let ic_arrow_bottom = DesignSystemAsset.Assets.icArrowBottom.swiftUIImage
+    public static let ic_arrow_back = DesignSystemAsset.Assets.icArrowBack.swiftUIImage
+    public static let ic_arrow_after = DesignSystemAsset.Assets.icArrowAfter.swiftUIImage
+    public static let ic_xmark = DesignSystemAsset.Assets.icXmark
 }
