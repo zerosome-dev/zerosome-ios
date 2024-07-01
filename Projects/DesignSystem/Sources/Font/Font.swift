6551ead29b63e0abbeccad8105bb4d7edbae1da6
Park Seo Yeon <syss220211@gmail.com>
2024-07-01 20:35:00 +0900

    🩹 [Fix] Fix for a issue of merge

--- Projects/DesignSystem/Sources/Font/Font.swift
+++ Projects/DesignSystem/Sources/Font/Font.swift
@@ -5,10 +5,7 @@
 //  Created by 박서연 on 2024/05/12.
 //  Copyright © 2024 iOS. All rights reserved.
 //
-<<<<<<< HEAD
 import UIKit
-=======
->>>>>>> origin/dev
 import SwiftUI
 
 public enum ZSFont {
